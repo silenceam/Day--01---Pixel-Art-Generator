d10b133ec8a2624cff0ccea65e94dcf52fe0f482
Aung Myat <aungmyat321995@gmail.com>
2023-07-13 17:07:03 +0700

    Yes

---
+++ script.js
@@ -0,0 +1,11 @@
+let container = document.querySelector(".container");
+let gridButton = document.querySelector("submit-grid");
+let clearGridButton = document.querySelector("clear-grid");
+let gridWidth = document.querySelector("width-range");
+let gridHeight = document.querySelector("height-range");
+let colorButton = document.querySelector("color-input");
+let eraseBtn = document.querySelector("erase-btn");
+let paintBtn = document.querySelector("paint-btn");
+let widthValue = document.querySelector("width-value");
+let heightValue = document.querySelector("height-value");
+
